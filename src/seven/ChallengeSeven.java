c8ca24d12d59cfeb774424620c8bce413797ea0d
John Sextro <john.sextro@asynchrony.com>
2018-04-11 15:25:38 -0500

    this made the processing just fast enough

--- src/seven/ChallengeSeven.java
+++ src/seven/ChallengeSeven.java
@@ -4,28 +4,29 @@ import java.util.ArrayList;
 
 public class ChallengeSeven {
     public static long findNthPrime(long nthPrime) {
-        long i = 1;
-        ArrayList<Long> primes = new ArrayList<Long>();
-
-        do {
-            if (isPrime(i))
-                primes.add(i);
-            i++;
-        } while (primes.size() < nthPrime);
-
-        return primes.get((int) (nthPrime-1));
+        if (nthPrime < 2) return 2;
+        if (nthPrime == 2) return 3;
+        if (nthPrime == 3) return 5;
+        nthPrime -= 2;
+        int[] primes = new int[(int) nthPrime];
+        primes[0] = 5;
+        int candidate, count, step = 2;
+        for(candidate = 7, count = 1; count < nthPrime; step = 6-step, candidate += step) {
+            if (isPrime(candidate,primes,count)) {
+                primes[count] = candidate;
+                ++count;
+            }
+        }
+        return primes[(int) (nthPrime-1)];
     }
 
-    private static boolean isPrime(long n)
-    {
-        if (n <= 1)
-            return false;
-        int m = (int)Math.sqrt(n) + 1;
-
-        for (long i = 2; i < m; ++i)
-            if (n % i == 0)
+    private static boolean isPrime(int n, int[] primes, int primeCount) {
+        int m = (int) Math.sqrt(n) + 1;
+        for (int i = 0, p; i < primeCount && (p = primes[i]) < m; ++i) {
+            if (n % p == 0) {
                 return false;
-
+            }
+        }
         return true;
     }
 }
